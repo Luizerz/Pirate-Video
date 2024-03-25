54a1413323b91b795b684d9327e794837dd75c99
Luizerz <senaluiz1307@gmail.com>
2024-03-25 15:55:42 -0300

    adding content on detail view

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -10,11 +10,26 @@
       class="absolute items-center gap-5 justify-center flex flex-col"
       v-if="obj"
     >
-      <iframe
-        width="560"
-        height="315"
-        :src="trailer"
-      ></iframe>
+      <h1
+        class="text-4xl font-bold font-Roboto text-white text-start self-start"
+      >
+        {{ props.type == "tv" ? obj.name : obj.title }}
+      </h1>
+      <div class="self-start flex justify-center items-center gap-3">
+        <h3 class="text-lg font-bold font-Roboto text-slate-300 text-start">
+          {{
+            props.type == "tv"
+              ? obj.first_air_date.split("-")[0]
+              : obj.release_date.split("-")[0]
+          }}
+        </h3>
+        <h3 class="text-green-300 font-Roboto">
+          <i class="fa-solid fa-star"></i>
+          {{ obj.vote_average }}
+        </h3>
+        <!-- <h3>{{ obj.runtime }}</h3> -->
+      </div>
+      <iframe width="560" height="315" :src="trailer"></iframe>
       <button
         v-if="!isFavorited"
         class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden font-bold rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 text-gray-300 hover:text-white font-Roboto text-xl"
@@ -24,6 +39,17 @@
           class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
         >
           Salvar <i class="fa-solid fa-heart-circle-plus"></i>
+        </span>
+      </button>
+      <button
+        v-if="isFavorited"
+        class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden font-bold rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 text-gray-300 hover:text-white font-Roboto text-xl"
+        @click="remove(obj)"
+      >
+        <span
+          class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
+        >
+          Remover <i class="fa-solid fa-heart-crack"></i>
         </span>
       </button>
     </div>
@@ -44,18 +70,20 @@ const save = () => {
   storage.addItem(obj.value);
   router.go();
 };
+const remove = () => {
+  storage.removeByID(obj.value.id);
+  router.go();
+};
 
 await getDataByID(props.item, props.type).then((data) => (obj.value = data));
-await getTrailerByID(props.item, props.type).then(
-  (data) => {
-    if (data) {
-      trailer.value = 'https://www.youtube.com/embed/'+ data
-    } else {
-      trailer.value = 'https://www.youtube.com/embed/'
-    }
+await getTrailerByID(props.item, props.type).then((data) => {
+  if (data) {
+    trailer.value = "https://www.youtube.com/embed/" + data;
+  } else {
+    trailer.value = "https://www.youtube.com/embed/";
   }
-);
-
+});
+console.log(obj.value);
 const imageUrl = (obj) => {
   return "https://image.tmdb.org/t/p/original" + obj.backdrop_path;
 };
