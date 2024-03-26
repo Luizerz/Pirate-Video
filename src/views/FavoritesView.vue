33689a9c771f6d0df6b276f5837662c843c5b0ad
Luizerz <senaluiz1307@gmail.com>
2024-03-25 21:36:39 -0300

    release - 8

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid grid-cols-2 md:grid-cols-5 gap-10 py-4 px-12 self-center"
+    class="grid grid-cols-2 md:grid-cols-5 gap-10 py-4 px-24 self-center"
     v-if="!(favoriteList.length == 0)"
   >
     <CardView
